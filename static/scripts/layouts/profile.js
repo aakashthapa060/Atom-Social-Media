5162dbec773baa4fb3ab40e0ae84646e4a1596ec
Aakash Thapa <aakashmanm3@gmail.com>
2021-06-03 12:10:55 +0545

    Porfile Section Modified and made Responsive

--- static/scripts/layouts/profile.js
+++ static/scripts/layouts/profile.js
@@ -1,8 +1,8 @@
-
+// Cover Pic
 let cover_pic = document.querySelector(".profile-container .profile-section-main .profile-section .profile-main-part .profile-part-cover-img img");
+
 function cover_change(){
-    randomNum = Math.floor(Math.random() * 6) + 1;
-    randomImg = "default" + randomNum + ".jpg";
-    cover_pic.src = `https://source.unsplash.com/featured/?aesthetic,nepal`
+    cover_pic.src = `https://source.unsplash.com/1600x900/?art,nature`;
+    alert(randomNum)
 }
 cover_change()
